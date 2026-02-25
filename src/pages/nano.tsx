56acc8dcf6cbbc0c84275fb9f937ff61a7e937d8
softgenai <admin@softgen.ai>
2026-02-25 08:39:09 +0000

    Updates

--- src/pages/nano.tsx
+++ src/pages/nano.tsx
@@ -1,12 +1,15 @@
 import { NanoCommandCenter } from "@/components/NANO/NanoCommandCenter";
 import { ProtectedRoute } from "@/components/ProtectedRoute";
+import { AppLayout } from "@/components/Layout/AppLayout";
 
 export default function NanoPage() {
   return (
-    <AppLayout>
-      <div className="flex-1 w-full h-[calc(100vh-4rem)] overflow-hidden">
-        <NanoCommandCenter />
-      </div>
-    </AppLayout>
+    <ProtectedRoute>
+      <AppLayout>
+        <div className="flex-1 w-full h-[calc(100vh-4rem)] overflow-hidden">
+          <NanoCommandCenter />
+        </div>
+      </AppLayout>
+    </ProtectedRoute>
   );
 }
